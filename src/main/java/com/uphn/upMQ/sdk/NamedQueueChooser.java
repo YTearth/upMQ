1b4288c4421306c7f46a1aee0caf2940cdd07bae
upChat <ttyyhagda@163.com>
2017-07-21 21:28:04 +0800

    [v1.0][添加主干体]

--- src/main/java/com/uphn/upMQ/sdk/NamedQueueChooser.java
+++ src/main/java/com/uphn/upMQ/sdk/NamedQueueChooser.java
@@ -1,5 +1,47 @@
 package com.uphn.upMQ.sdk;
 
-public class NamedQueueChooser {
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * @author tianyin
+ *
+ */
+public class NamedQueueChooser {
+    
+	private Map<String, MessageQueue> queueMap;
+	private static NamedQueueChooser INSTANCE = null;
+	
+	private NamedQueueChooser() {
+		queueMap = new ConcurrentHashMap<String, MessageQueue>();
+	}
+	
+	public static NamedQueueChooser getInstance() {
+		if (INSTANCE == null) {
+			synchronized (NamedQueueChooser.class) {
+				if (INSTANCE == null) {
+					INSTANCE = new NamedQueueChooser();
+				}
+			}
+		}
+		return INSTANCE;
+	}
+	/**
+	 * 生产方拿队列,若队列不存在则创建。消费方拿队列，若队列不存在则返回null
+	 * @param queueName
+	 * @param autoCreate
+	 * @return
+	 */
+	public MessageQueue fetctMessageQueue(String queueName, boolean autoCreate) {
+		if (queueName == null) {
+			return null;
+		}
+		MessageQueue messageQueue = queueMap.get(queueName);
+		//TODO 当前采用ConcurrentHashMap保证线程安全,可选仅在messageQueue ==null加同步锁,后者实际效率可能会更高,待测。
+		if (messageQueue == null && autoCreate) { 
+			messageQueue = new MessageQueue(queueName);
+			queueMap.put(queueName, messageQueue);
+		}
+		return messageQueue;
+	}
 }
